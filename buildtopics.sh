28cad83eb59fabb95d8e75b506ae296a00a86d20
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-10-21 12:30:51 -0400

    Create buildtopics.sh

---
+++ buildtopics.sh
@@ -0,0 +1,10 @@
+source source.sh
+bin/pulsar-admin topics create persistent://public/default/transcom
+
+bin/pulsar-admin topics create persistent://public/default/weather
+
+bin/pulsar-admin topics create persistent://public/default/energy
+
+bin/pulsar-admin topics create persistent://public/default/iotjetsonjson
+
+bin/pulsar-admin topics create persistent://public/default/iot3
